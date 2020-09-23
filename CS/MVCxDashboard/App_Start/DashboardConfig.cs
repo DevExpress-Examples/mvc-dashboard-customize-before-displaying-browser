9514d2153350f917ba62d69015887ab249c7045c
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2020-09-23 15:13:46 +0300

    Update to 20.1.7

--- CS/MVCxDashboard/App_Start/DashboardConfig.cs
+++ CS/MVCxDashboard/App_Start/DashboardConfig.cs
@@ -1,21 +1,19 @@
-using System.Web.Routing;
+using DevExpress.DashboardCommon;
 using DevExpress.DashboardWeb;
 using DevExpress.DashboardWeb.Mvc;
-using DevExpress.DashboardCommon;
-using System.Xml.Linq;
-using System.Linq;
-using System.Data;
 using DevExpress.DataAccess.Sql;
+using System.Linq;
+using System.Web.Routing;
+using System.Xml.Linq;
 
 namespace MVCxDashboard {
     public class DashboardConfig {
         public static void RegisterService(RouteCollection routes) {
             routes.MapDashboardRoute();
 
+            DashboardConfigurator.Default.AllowExecutingCustomSql = true;
             DashboardConfigurator.Default.SetDashboardStorage(new MyDashboardFileStorage(@"~/App_Data/Dashboards/"));
-
         }
-    
     }
 
     public class MyDashboardFileStorage : DashboardFileStorage {
@@ -30,15 +28,14 @@ namespace MVCxDashboard {
                 parameterCountry.Value = "Germany";
             }
             var nwindDataSource = d.DataSources.OfType<DashboardSqlDataSource>().FirstOrDefault(ds => ds.Name == "NwindDataSource");
-            if( nwindDataSource != null) {
+            if (nwindDataSource != null) {
                 var customNwindQuery = nwindDataSource.Queries.OfType<CustomSqlQuery>().FirstOrDefault(q => q.Name == "CustomInvoicesQuery");
-                if(customNwindQuery != null) {
+                if (customNwindQuery != null) {
                     customNwindQuery.Sql += " AND (Invoices.OrderDate >= #2015-01-01 00:00:00#)";
                 }
             }
-            
+
             return d.SaveToXDocument();
         }
     }
-
 }
